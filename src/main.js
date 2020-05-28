8ad0b651c3f4ba1f6a364f2a9996b85622eb922d
LoretoGutierrezH <loretocgh@gmail.com>
2020-05-28 12:36:24 -0400

    Estructura básica mobile (en index.html por el momento) + un poco de javascript básico de lectura y escritura de datos en la base de datos, solo como ejemplo

--- src/main.js
+++ src/main.js
@@ -1,5 +1,32 @@
 // Este es el punto de entrada de tu aplicacion
+import { db } from './lib/index.js';
 
-import { myFunction } from './lib/index.js';
+const root = document.querySelector('.root');
 
-myFunction();
+//Crear estructura de la página
+const mainStructure = function() {
+  root.innerHTML = `
+    <div>Hola</div>
+  
+  `
+}
+
+//Leer publicaciones
+db.collection('posts').onSnapshot(snapshot => {
+  snapshot.docs.forEach(doc =>{
+    let document = doc;
+    let docData = doc.data();
+    console.log(docData); 
+  });
+})
+
+
+//Crear publicaciones
+/* db.collection('posts').add({
+  title: "Quiero un perro",
+  content: "Hace mucho que no tengo un perrito, ¿qué raza me recomiendan?"
+})  */
+
+//Eliminar publicaciones
+
+//Asociar publicaciones a usuarios
