bf02a06f97f2541da29a9739ae755d3d6714945b
Nadia080290 <ngallardome@gmail.com>
2020-06-03 10:44:54 -0400

    aceptando cambios

---
+++ src/lib/router.js
@@ -0,0 +1,18 @@
+import { home } from './views/home.js';
+ 
+export const manageRoute = (hash) => {
+ if( hash === '#/home'){
+     return showPage(hash)
+ else if ( hash === )    
+ }
+
+const showPage(hash)
+
+
+{
+  swicht (hash)
+  window.location
+}
+}
+
+
