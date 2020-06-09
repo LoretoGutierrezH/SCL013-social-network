b3a97ae0cdfa5c44042d9eb4732d7d4eccacb9c8
Nadia080290 <ngallardome@gmail.com>
2020-06-09 13:00:15 -0400

    agregando cambios

--- src/lib/views/aboutUs.js
+++
@@ -1,8 +0,0 @@
-export const aboutUs = () => {
-  const aboutUsView = `
-    <h1>Categoría de la publicación</h1>
-    <h4>Primera publiació</h4>
-    <p>Contenido de la publicación</p>
-  `;
-  return aboutUsView;
-}
