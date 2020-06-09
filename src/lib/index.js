3d05c2d3979d1e51a69975b4eef50eba2ad7d96e
Nadia080290 <ngallardome@gmail.com>
2020-06-09 10:21:51 -0400

    aceptando js

--- src/lib/index.js
+++
@@ -1,5 +0,0 @@
-// aqui exportaras las funciones que necesites
-export const db = firebase.firestore();
-export const auth = firebase.auth();
-
-// Autenticación y creación de documentos de collection users
